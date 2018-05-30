f6c1d2537ded02701958896f66b79c293516af7d
Tong <temberature@gmail.com>
2018-05-30 11:15:47 +0800

    bubbleSort标准版。

--- src/sort/bubbleSort.js
+++ src/sort/bubbleSort.js
@@ -1,24 +1,20 @@
-var bubbleSort = function(a) {
-  var tmp;
-  for (var j = 1; j < a.length; j++) {
-    for (var i = 0; i < a.length - j; i++) {
-      if (a[i] > a[i + 1]) {
-        tmp = a[i];
-        a[i] = a[i + 1];
-        a[i + 1] = tmp;
+var comparator = function(a, b) {
+  return a - b;
+}
+
+var bubbleSort = function(a, cmp) {
+  cmp = cmp || comparator;
+  var temp;
+  for (var i = 0; i < a.length; i++) {
+    for (var j = i; j > 0; j--) {
+      if (cmp(a[j], a[j - 1]) < 0) {
+        temp = a[j];
+        a[j] = a[j - 1];
+        a[j - 1] = temp;
       }
     }
   }
   return a;
-};
-
-var bubbleSortR = function(a) {
-  var bubbleIter = function(a, k) {
-    return a[k] > a[k + 1]
-      ? a.slice(0, k).concat(a[k + 1], a[k], a.slice(k + 2))
-      : bubbleIter(a, k + 1);
-  };
-  return bubbleIter(a, 1);
 };
 
 module.exports = bubbleSort;
