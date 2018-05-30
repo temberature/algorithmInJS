967c75f101b79a42ce32d40ada8e92eb045d3ee6
TongZhang <temberature@gmail.com>
2018-05-30 08:59:10 +0800

    临时提交

--- src/sort/bubbleSort.js
+++ src/sort/bubbleSort.js
@@ -1,5 +1,3 @@
-
-
 var bubbleSort = function(a) {
   var tmp;
   for (var j = 1; j < a.length; j++) {
@@ -12,6 +10,15 @@ var bubbleSort = function(a) {
     }
   }
   return a;
+};
+
+var bubbleSortR = function(a) {
+  var bubbleIter = function(a, k) {
+    return a[k] > a[k + 1]
+      ? a.slice(0, k).concat(a[k + 1], a[k], a.slice(k + 2))
+      : bubbleIter(a, k + 1);
+  };
+  return bubbleIter(a, 1);
 };
 
 module.exports = bubbleSort;
